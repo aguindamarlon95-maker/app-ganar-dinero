95738e799a9287d2042e58804ac4872a7f897fc3
Marlon <megatienda12@gmail.com>
2026-02-08 14:38:06 -0500

    Agregando STATIC_ROOT para Render

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +121,5 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Configuración para archivos estáticos (CSS, JS, Imágenes)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
