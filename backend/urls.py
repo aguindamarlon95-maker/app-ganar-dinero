1555fa8098cbad11e33a3d175070d24c1cf3dcf4
Marlon <megatienda12@gmail.com>
2026-02-08 15:32:13 -0500

    Creando atajo para subir fotos

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import subir_evidencia_web  # <--- IMPORTANTE: Traemos la función aquí
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # ESTA LÍNEA ES LA CLAVE: Conecta el servidor con tu app
-    path('api/', include('core.urls')),
+    path('api/', include('core.urls')),     # Las rutas de datos siguen aquí
+    path('subir/', subir_evidencia_web),    # <--- ¡EL ATAJO NUEVO! (Directo al grano)
 ]
