bf01bb025f6749e769aa744a9c45ea1e8ec71786
Marlon <megatienda12@gmail.com>
2026-02-08 17:07:34 -0500

    Habilitando vista de fotos media

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import subir_evidencia_web  # <--- IMPORTANTE: Traemos la función aquí
+from core.views import subir_evidencia_web
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),     # Las rutas de datos siguen aquí
-    path('subir/', subir_evidencia_web),    # <--- ¡EL ATAJO NUEVO! (Directo al grano)
+    path('api/', include('core.urls')),
+    path('subir/', subir_evidencia_web),
 ]
+
+# ESTO ES LO NUEVO: Permite ver las fotos subidas
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
